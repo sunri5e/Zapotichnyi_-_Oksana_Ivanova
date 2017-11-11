7319dabfb7856e15ebd9cf51d0f4087d249ba995
Sasha Hladyshchuk <s_hladyshchuk@admins-MacBook-Pro.local>
2017-11-11 18:48:35 +0200

    release

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 (function($){
-  $(window).on("load",function(){
+  $(document).ready(function() {
 
     // Popup
     function bodyScroll(bool) {
       if (bool === true) {
         $('body').removeClass('mod-disable-scroll')
+        $('.oi-popup').scrollTop(0)
       } else if (bool === false) {
         $('body').addClass('mod-disable-scroll');
       } else {
@@ -18,25 +19,26 @@
       let gallerySrc = $(this).children('.oi-thumb--slider-src').children().clone()
 
       $('[data-gallery] .oi-slider').append(gallerySrc);
-
-      $(".oi-slider").mCustomScrollbar({
-        axis: "x",
-        theme: "dark-3",
-        advanced: { 
-          autoExpandHorizontalScroll: true
-        }
-      });
+      if ($(window).width() > 1024) {
+        $(".oi-slider").mCustomScrollbar({
+          axis: "x",
+          theme: "dark-3",
+          advanced: { 
+            autoExpandHorizontalScroll: true
+          }
+        });
+      }
+      $('[data-gallery]').addClass('mod-open');
 
       bodyScroll(false);
-      $('[data-gallery]').addClass('mod-open');
     });
 
-    // $('[data-open-popup]').on('click', function(e){
-    //   e.preventDefault();
+    $('[data-open-popup]').on('click', function(e){
+      e.preventDefault();
 
-    //   bodyScroll(false);
-    //   $('[data-gallery]').addClass('mod-open');
-    // });
+      bodyScroll(false);
+      $($(this).data('open-popup')).addClass('mod-open');
+    });
 
     $('[data-close-popup]').on('click', function(e){
       e.preventDefault();
@@ -48,5 +50,63 @@
       $($(this).parents('.oi-popup')).removeClass('mod-open');
     });
     // end Popup
+
+
+    // Form submit
+    $('#footerContacts').submit(function(e) {
+      var name = document.getElementById('footerFormName'),
+          phone = document.getElementById('footerFormPhone'),
+          message = document.getElementById('footerFormMessage');
+
+      if (!name.value || !phone.value || !message.value) {
+        console.log('Please check your entries');
+        return false;
+      } else {
+        e.preventDefault();
+        $.ajax({
+          url: "https://formspree.io/sunrise944@gmail.com", 
+          method: "POST",
+          data: $(this).serialize(),
+          dataType: "json"
+        });
+        $(this).get(0).reset();
+        $('#formSuccess').addClass('mod-open');
+        return console.log('Message sent');
+      }
+    });
+
+
+    $('#consultContacts').submit(function(e) {
+      var name = document.getElementById('consultFormName'),
+          phone = document.getElementById('consultFormPhone'),
+          email = document.getElementById('consultFormMail'),
+          message = document.getElementById('consultFormMessage');
+
+      if (!name.value || !phone.value || !email.value || !message.value) {
+        console.log('Please check your entries');
+        return false;
+      } else {
+        e.preventDefault();
+        $.ajax({
+          url: "https://formspree.io/sunrise944@gmail.com", 
+          method: "POST",
+          data: $(this).serialize(),
+          dataType: "json"
+        });
+        $(this).get(0).reset();
+        $('#formSuccess').addClass('mod-open');
+        $($(this).parents('.oi-popup')).removeClass('mod-open');
+        return console.log('Message sent');
+      }
+    });
+    // end Form submit
+
+    $('.oi-form-control').keyup(function() {
+      if ($(this).val()) {
+        $(this).addClass('mod-filled')
+      } else {
+        $(this).removeClass('mod-filled')
+      }
+    });
   });
 })(jQuery);
