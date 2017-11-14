49b0f38aa26a866d213f557723b15fe6156fba5a
Sasha Hladyshchuk <s_hladyshchuk@admins-MacBook-Pro.local>
2017-11-14 12:21:12 +0200

    slider fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 (function($){
-  $(document).ready(function() {
+  $(window).on("load", function() {
 
     // Popup
     function bodyScroll(bool) {
@@ -13,33 +13,6 @@
       }
     }
 
-    $('[data-open-gallery]').on('click', function(e){
-      e.preventDefault();
-
-      let gallerySrc = $(this).children('.oi-thumb--slider-src').children().clone(),
-          galleryName = $(this).children('.oi-thumb--title').text(),
-          gallerySquare = $(this).children('.oi-thumb--descr').text(),
-          galleryTitle = galleryName + '  /  ' + gallerySquare;
-
-          console.log(galleryTitle);
-
-      $('[data-gallery] .oi-slider').append(gallerySrc);
-      $('[data-gallery] .oi-popup--header h4').html(galleryTitle);
-
-      if ($(window).width() > 1024) {
-        $(".oi-slider").mCustomScrollbar({
-          axis: "x",
-          theme: "dark-3",
-          advanced: { 
-            autoExpandHorizontalScroll: true
-          }
-        });
-      }
-      $('[data-gallery]').addClass('mod-open');
-
-      bodyScroll(false);
-    });
-
     $('[data-open-popup]').on('click', function(e){
       e.preventDefault();
 
@@ -49,9 +22,6 @@
 
     $('[data-close-popup]').on('click', function(e){
       e.preventDefault();
-
-      $("[data-open-gallery] .oi-slider").mCustomScrollbar("destroy");
-      $('[data-gallery] .oi-slider').html('')
 
       bodyScroll(true);
       $($(this).parents('.oi-popup')).removeClass('mod-open');
@@ -74,7 +44,6 @@
           message = document.getElementById('footerFormMessage');
 
       if (!name.value || !phone.value || !message.value) {
-        console.log('Please check your entries');
         return false;
       } else {
         e.preventDefault();
@@ -86,7 +55,7 @@
         });
         $(this).get(0).reset();
         showSuccess('.oi-section--success');
-        return console.log('Message sent');
+        return
       }
     });
 
@@ -98,7 +67,6 @@
           message = document.getElementById('consultFormMessage');
 
       if (!name.value || !phone.value || !email.value || !message.value) {
-        console.log('Please check your entries');
         return false;
       } else {
         e.preventDefault();
@@ -111,7 +79,7 @@
         $(this).get(0).reset();
         showSuccess('#formSuccess');
         $($(this).parents('.oi-popup')).removeClass('mod-open');
-        return console.log('Message sent');
+        return;
       }
     });
 
@@ -123,7 +91,6 @@
           message = document.getElementById('orderDesignFormMessage');
 
       if (!name.value || !phone.value || !email.value || !message.value) {
-        console.log('Please check your entries');
         return false;
       } else {
         e.preventDefault();
@@ -136,7 +103,7 @@
         $(this).get(0).reset();
         showSuccess('#formSuccess');
         $($(this).parents('.oi-popup')).removeClass('mod-open');
-        return console.log('Message sent');
+        return;
       }
     });
 
@@ -148,7 +115,6 @@
           message = document.getElementById('orderServiceFormMessage');
 
       if (!name.value || !phone.value || !email.value || !message.value) {
-        console.log('Please check your entries');
         return false;
       } else {
         e.preventDefault();
@@ -161,7 +127,7 @@
         $(this).get(0).reset();
         showSuccess('#formSuccess');
         $($(this).parents('.oi-popup')).removeClass('mod-open');
-        return console.log('Message sent');
+        return;
       }
     });
     // end Form submit
